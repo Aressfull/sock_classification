1e9ad6ce44ba7477499fb40c9c5c385d0e386ba8
z5163449 <30581408+z5163449@users.noreply.github.com>
2023-01-02 14:28:23 +0800

    update config.py

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ parser.add_argument('--subsample', type=int, default=1)
 
 parser.add_argument('-epoch','--n_epoch', type=int, default=60)
 parser.add_argument('-extra_data','--extra_data', type=int, default=0)
-parser.add_argument('-l1','--lambda_L1', type=float, default=1e-4)
+parser.add_argument('-l1','--lambda_L1', type=float, default=0)
 parser.add_argument('-lr','--lr', type=float, default=1e-3)
 parser.add_argument('-l2','--weight_decay', type=float, default=1e-4)
 parser.add_argument('-batch','--batch_size', type=int, default=64)
@@ -34,6 +34,8 @@ parser.add_argument('-patch_size','--patch_size', type=int, default=4)
 parser.add_argument('-pretrain','--pretrain', type=int, default=1)
 parser.add_argument('-encoder_dims','--hidden_size', type=int, default=768)
 parser.add_argument('-encoder_att_heads','--encoder_att_heads', type=int, default=12)
+parser.add_argument('-comparisons','--comparisons', type=int, default=30)
+parser.add_argument('-beta','--beta', type=float, default=0.5)
 
 
 def gen_args(model="ViT"):
@@ -54,4 +56,3 @@ def gen_args(model="ViT"):
     os.system('mkdir -p ' + args.rec_path)
 
     return args
-
