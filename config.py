44104cc9ddcfb6c16d68139801dd3a3853c59020
z5163449 <30581408+z5163449@users.noreply.github.com>
2023-01-06 10:44:04 +0800

    changed config

--- config.py
+++ config.py
@@ -27,7 +27,7 @@ parser.add_argument('--log_per_iter', type=int, default=10000)
 
 parser.add_argument('-depth','--num_hidden_layers', type=int, default=6)
 parser.add_argument('-mask','--mask_ratio', type=float, default=0.1)
-parser.add_argument('-n_pretrain','--n_pretrain', type=int, default=parser.parse_known_args()[0].n_epoch)
+parser.add_argument('-n_pretrain','--n_pretrain', type=int, default=60)
 parser.add_argument('-mask_type','--mask_type', type=str, default='tube')
 parser.add_argument('-tubelet_size','--tubelet_size', type=int, default=5)
 parser.add_argument('-patch_size','--patch_size', type=int, default=4)
@@ -36,7 +36,7 @@ parser.add_argument('-encoder_dims','--hidden_size', type=int, default=768)
 parser.add_argument('-encoder_att_heads','--encoder_att_heads', type=int, default=12)
 parser.add_argument('-comparisons','--comparisons', type=int, default=30)
 parser.add_argument('-beta','--beta', type=float, default=0.5)
-
+parser.add_argument('-lr_pretrain','--lr_pretrain',type=float,default=0.005)
 
 def gen_args(model="ViT"):
     args, unknown = parser.parse_known_args()
